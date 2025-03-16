b6715a467d5626cd0f79d434eb37cfc0f8d839eb
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-03-16 23:44:39 +0000

    añadido el reloj

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,19 @@
+// src/js/components/Home.jsx
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="bigCounter">
+      <div className="calendar">
+	  <i className="fa-regular fa-clock"></i>
+      </div>
+      <div className="four">0</div>
+      <div className="three">0</div>
+      <div className="two">0</div>
+      <div className="one">0</div>
+    </div>
+  );
 };
 
 export default Home;
